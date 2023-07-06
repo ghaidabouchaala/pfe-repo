2af524e4e1fbe6919a2cf951832ecff31305697c
ghaidabouchaala <ghaida.bouchaala@gmail.com>
2023-07-06 09:07:42 +0200

    link data acquisition and anomaly detection

--- wsn_system/main.m
+++ wsn_system/main.m
@@ -1,28 +1,20 @@
-% Start the data acquisition and processing in a new thread
-f = parfeval(@run, 0, 'data_processing_storage.m');
+clear all;
 
-% Pause for some time (e.g., 10 minutes) to allow data accumulation
-pause(10*60);
+% Start the data acquisition and processing in a new thread
+data_processing_future = parfeval(@data_processing_storage, 1);
+
+% Pause for some time (e.g., 5 minutes) to allow data accumulation
+pause(2*60);
+
+output = fetchOutputs(data_processing_future);
+disp(output);
 
 % Start the anomaly detection in parallel for all senders
-parpool(4); % Start a parallel pool with 4 workers
+p = gcp('nocreate'); % If no pool, do not create new one.
+if isempty(p)
+    parpool(4); % Start a parallel pool with 4 workers
+end
 parfor i = 1:4
-    detect_anomalies(['sender', num2str(i)]);
+    detect_anomalies(['sender', num2str(i)], data_processing_future);
 end
 delete(gcp); % Close the parallel pool
-
-% After all the detections have finished, calculate metrics
-calculate_metrics();
-
-% Check if the data acquisition and processing is still running and cancel it if it is
-if ~strcmp(f.State, 'finished')
-    cancel(f);
-end
-
-% Delete the serial port object
-if isvalid(port)
-    delete(port);
-    disp('Serial port connection closed.');
-else
-    disp('Serial port connection is already closed or does not exist.');
-end
